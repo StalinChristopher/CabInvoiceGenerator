21328ac56c1ffebe7faa22209e31e53ab24aefcb
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-28 18:43:29 +0530

    Initialized maven project

---
+++ src/main/java/com/yml/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -0,0 +1,13 @@
+package com.yml.CabInvoiceGenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class CabInvoiceGenerator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Cab Invoice generator program" );
+    }
+}
---
+++ src/test/java/com/yml/CabInvoiceGenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,38 @@
+package com.yml.CabInvoiceGenerator;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CabInvoiceGeneratorTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CabInvoiceGeneratorTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CabInvoiceGeneratorTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
