b1d25f725c78b45db9d89705dee16a2d6c9070f4
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-29 01:07:44 +0530

    added STEP5 for generating invoice for given userID based on ride type

--- src/main/java/com/yml/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/main/java/com/yml/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -9,7 +9,16 @@ import java.util.List;
  */
 public class CabInvoiceGenerator {
 	
-	private final double MIN_FARE = 5;
+	private final int NORMAL_MIN_FARE = 5;
+	private final int PREMIUM_MIN_FARE = 20;
+    private final int NORMAL_FARE_PER_KM = 10;
+    private final int PREMIUM_FARE_PER_KM = 15;
+    private final int NORMAL_FARE_PER_MIN = 1;
+    private final int PREMIUM_FARE_PER_MIN = 2;
+	
+	enum RIDE {
+		NORMAL, PREMIUM
+	};
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Cab Invoice generator program" );
@@ -21,11 +30,18 @@ public class CabInvoiceGenerator {
      * @return
      * Method to generate fare for given distance and time
      */
-    public double generateFare(double distance, int time) {
+    public double generateFare(RIDE ride,double distance, int time) {
     	double fare = 0;
-    	fare = (distance *10) + (time * 1);
-    	if( fare < 5) {
-    		fare = MIN_FARE;
+    	if(ride == RIDE.NORMAL ) {
+    		fare = (distance * NORMAL_FARE_PER_KM) + (time * NORMAL_FARE_PER_MIN);
+        	if( fare < 5) {
+        		fare = NORMAL_MIN_FARE;
+        	}
+    	}else {
+    		fare = (distance * PREMIUM_FARE_PER_KM) + (time * PREMIUM_FARE_PER_MIN);
+        	if( fare < 5) {
+        		fare = PREMIUM_MIN_FARE;
+        	}
     	}
     	return fare;
     	
@@ -39,7 +55,7 @@ public class CabInvoiceGenerator {
 	public double generateAggregateFare(List<Ride> rides) {
 		double totalFare = 0;
 		for(Ride ride : rides) {
-			totalFare += generateFare(ride.getDistance(), ride.getTime());
+			totalFare += generateFare(ride.getRide(),ride.getDistance(), ride.getTime());
 		};
 		return totalFare;
 		
@@ -48,7 +64,7 @@ public class CabInvoiceGenerator {
 	public Invoice generateInvoice(List<Ride> rides) {
 		double totalFare = 0;
 		for(Ride ride : rides) {
-			totalFare += generateFare(ride.getDistance(), ride.getTime());
+			totalFare += generateFare(ride.getRide(),ride.getDistance(), ride.getTime());
 		};
 		Invoice invoice = new Invoice(totalFare,rides.size());
 
